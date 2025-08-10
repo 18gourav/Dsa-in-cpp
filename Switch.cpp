ebc143b4abbd4c1fde4b7585541ec5d23d8fadc9
18gourav <gouravgarg993@gmail.com>
2025-08-10 22:53:56 +0530

    Implement code changes to enhance functionality and improve performance

---
+++ Switch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+   int no = 1;
+
+   switch(no) {
+        case 1 : cout << "this is no 1" << endl;
+        break;
+
+        case 2: cout<<"this is no 2"<<endl;
+        break;
+
+        default: cout<<"this is default case";
+        
+   }
+
+   return 0;
+}

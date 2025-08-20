948ae37ea93d3c676eff2a5e23e278752be314fe
18gourav <gouravgarg993@gmail.com>
2025-08-20 20:21:28 +0530

    did insertion sort

---
+++ Sorting/insertionSort.cpp
@@ -0,0 +1,23 @@
+// timecomplexity:
+//  best case: O(n)
+//  worst case: O(n^2)
+// use case- when the size of the array is small try to implement insertion sort
+// space complexity = O(1)
+
+#include <bits/stdc++.h> 
+void insertionSort(int n, vector<int> &arr){
+    // Write your code here.
+    for(int i = 1; i<n; i++){
+        int temp = arr[i];
+        int j = i-1;
+        for(; j>=0; j--){
+            if(arr[j] > temp ){
+                arr[j+1] = arr[j];
+            }
+            else {
+                break;
+            }
+        }
+        arr[j+1] = temp;
+    }
+}

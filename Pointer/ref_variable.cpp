ce359b619d615b73996308c56c77cd31881de7d3
18gourav <gouravgarg993@gmail.com>
2025-09-18 23:49:52 +0530

    Add reference variable demonstration with update functions in C++

---
+++ Pointer/ref_variable.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+void update2(int &n) {
+    n++;
+}
+
+void update(int n) {
+    n++;
+}
+
+int main() {
+    //what is reference varibale? --> it means have 2 variable pointing to same memory(eg - i,j pointing to 5)
+
+    int i = 6;
+    //this is syntax how reference variable created
+    int &j = i;
+
+    cout<<i<<endl;
+    i++;
+    cout<<i<<endl;
+    j++;
+    cout<<i<<endl;
+
+    //here a qsn arise --> Why we need it?
+    //in this when we call update function the "i" was updated in the update func not in main function
+    cout<<i<<endl;
+    update(i);
+    cout<<i<<endl;
+
+//now here ref variable comes in play --> what if we pass ref var in update --> then the i will also update
+// in update2 and name but with diff names
+
+    cout<<i<<endl;
+    update2(i);
+    cout<<i<<endl;
+
+    return 0;
+}

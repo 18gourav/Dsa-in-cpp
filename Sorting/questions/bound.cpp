4b46e5f1330f33fca692ff6eec766e1e3f830cdf
18gourav <gouravgarg993@gmail.com>
2025-12-28 12:24:35 +0530

    revising qsns

--- Sorting/questions/bound.cpp
+++ Sorting/questions/bound.cpp
@@ -1,48 +1,63 @@
 #include<iostream>
 using namespace std;
 
-int Occurence(int arr[],int n,int k){
-    int s = 0,e=n-1;
-    int ans = -1;
-    int anss = -1;
+int firstOcuurence(int arr[],int n, int k){
+   int s = 0;
+   int e=n-1;
+   int ans = -1;
+   int mid = s + (e-s)/2;
 
-    int mid = s + (e-s)/2;
-
-    while(s<e){
-       if(arr[mid] == k){
-           ans = mid;
-           e = mid-1;
-       }
-       else if(k>arr[mid]){
-         s = mid+1;
-       }
-       else if(k<arr[mid]){
-          e = mid-1;
-       }
-
-       mid = s + (e-s)/2;
+   while(s<=e){
+    if(arr[mid] == k){
+        ans = mid;
+        e = mid-1;
     }
 
-    while(s<e){
-       if(arr[mid] == k){
-           anss = mid;
-           s = mid+1;
-       }
-       else if(k>arr[mid]){
-         s = mid+1;
-       }
-       else if(k<arr[mid]){
-          e = mid-1;
-       }
-
-       mid = s + (e-s)/2;
+    else if(k>arr[mid]){
+        s = mid+1;
     }
 
-    return anss;
-    
+    else if(k<arr[mid]){
+       e = mid-1;
+    }
+
+    mid = s + (e-s)/2;
+   }
+
+   return ans;
+}
+
+int secOcuurence(int arr[],int n, int k){
+   int s = 0;
+   int e=n-1;
+   int ans = -1;
+   int mid = s + (e-s)/2;
+
+   while(s<=e){
+    if(arr[mid] == k){
+        ans = mid;
+        s = mid+1;
+    }
+
+    else if(k>arr[mid]){
+        s = mid+1;
+    }
+
+    else if(k<arr[mid]){
+       e = mid-1;
+    }
+
+    mid = s + (e-s)/2;
+   }
+
+   return ans;
 }
 
 int main() {
-    int arr[5] = {1,2,3,3,5};
-    cout<<"occurence of 3 is "<<Occurence(arr,5,3);
+    int arr[5] = {1,2,2,3,5};
+    int first = firstOcuurence(arr,5,2);
+    int sec = secOcuurence(arr,5,2);
+
+    cout<<"first occ is "<<first<<endl<<"second occ is "<<sec;
+    
 }

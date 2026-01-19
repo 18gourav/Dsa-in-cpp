8fd7218bad8ef261b7a867795694b011c5e121b9
18gourav <gouravgarg993@gmail.com>
2026-01-19 23:42:21 +0530

    implemented double LL and circular ll

---
+++ linkedlist/circularLL/intro.cpp
@@ -0,0 +1,9 @@
+//the circular LL is of two types:
+
+//1.Single circular LL ----> In this all things remain like single LL, only one change that is the last node points to first node
+// instead of null which makes it circular
+
+//2.Double circular LL ----> In this all things remain like double LL, only one change that is last node point to first and first point to
+//last that makes it circular
+
+//In this file we will talk about the "Single circular LL"
--- linkedlist/doublell/intro.cpp
+++ linkedlist/doublell/intro.cpp
@@ -13,6 +13,15 @@ class Node {
         this->next=NULL;
         this->prev=NULL;
     }
+
+    ~Node() {
+        int value = this->data;
+        if(this->next!= NULL){
+            delete next;
+            this ->next = NULL;
+        }
+        cout <<"the memory is free for node contain data "<<value<<endl;
+    }
 };
 
 void printdll(Node* &head){
@@ -34,21 +43,109 @@ int getLen(Node *&head){
     return len;
 }
 
-void insertAtHead(Node* &head,int d){
-    Node* Node1 = new Node(d);
-    Node1->next = head;
-    head->prev=Node1;
-    head = Node1;
+void insertAtHead(Node* &head,Node* &tail,int d){
+    if(head == NULL){
+        Node* newNode = new Node(d);
+        head = newNode;
+        tail = newNode;
+    }
+    else{
+        Node* Node1 = new Node(d);
+        Node1->next = head;
+        head->prev=Node1;
+        head = Node1;
+    }
+    
+}
+
+void insertAttail(Node* &head,Node* &tail,int d){
+    if(tail == NULL){
+        Node* newNode = new Node(d);
+        tail = newNode;
+        head = newNode;
+    }
+    else {
+        Node * node2 = new Node(d);
+        node2->prev = tail;
+        tail->next=node2;
+        tail = node2;
+    }
+    
+}
+
+void insertAtposn(Node * &tail,Node * &head,int d,int posn){
+    if(posn == 1){
+        insertAtHead(head,tail,d);
+        return ;
+    }
+
+    Node * temp = head;
+    int cnt = 1;
+
+    while(cnt<posn-1){
+        temp = temp->next;
+        cnt++;
+    }
+
+    if(temp->next == NULL){
+        insertAttail(head,tail,d);
+        return ;
+    }
+
+    Node * node3 = new Node(d);
+    node3->next=temp->next;
+    temp->next->prev = node3;
+    node3->prev=temp;
+    temp->next=node3;
+}
+
+void deletingNode(Node* &head,int posn){
+    if(posn == 1){
+        Node* temp = head;
+        head = head->next;
+        temp->next=NULL;
+        head->prev=NULL;
+        delete temp;
+    }
+    else {
+        Node* curr = head;
+        Node* prev = NULL;
+        int cnt = 1;
+
+        while(cnt<posn){
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+
+        curr->prev = NULL;
+        prev->next=curr->next;
+        curr->next=NULL;
+
+        delete curr;
+    }
 }
 
 int main(){
-    Node *newNode = new Node(10);
 
-    Node *head = newNode;
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    insertAttail(head,tail,15);
     printdll(head);
 
-    insertAtHead(head,15);
+    insertAtHead(head,tail,16);
+    insertAttail(head,tail,28);
     printdll(head);
+
+    insertAtposn(tail,head,22,2);
+    printdll(head);
+
+    deletingNode(head,4);
+    printdll(head);
+
+    // cout<<head->data<<endl;
+    // cout<<tail->data<<endl;
 
     cout<<getLen(head)<<endl;
     return 0;

9a76ea3bcdfac3c7a6f9470b17c1be7480997ede
18gourav <gouravgarg993@gmail.com>
2026-01-20 23:55:01 +0530

    added implementation for single circular linked list with insert function

--- linkedlist/circularLL/intro.cpp
+++ linkedlist/circularLL/intro.cpp
@@ -7,3 +7,53 @@
 //last that makes it circular
 
 //In this file we will talk about the "Single circular LL"
+
+//'VERY IMP'
+//in this we only do 'Tail' because through that we can access tail and head both
+#include<iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+    }
+
+    ~Node(){
+        int value = this->data;
+        if(this->next!=NULL){
+            delete next;
+            next=NULL:
+        }
+        cout<<"memory free for node with data "<<value;
+    }
+}
+
+void insertElem(Node* &Tail,int elem,int d){
+    
+    //empty list
+    if(Tail==NULL){
+        Node * newNode = new Node(d);
+        tail = newNode;
+        newNode->next=newNode;
+    }
+    else{
+        //non empty list
+        //assuming that elem is present in the list
+
+        Node * curr = Tail;
+
+        while(curr->data != elem){
+            curr = curr->next;
+        }
+
+        //element founded and curr is on that elem
+        Node* Node1 = new Node(d);
+        Node1->next=curr->next;
+        curr->next=Node1;
+    }
+}

b23822c78e70e217edb610e425ffe8ac3118f42c
18gourav <gouravgarg993@gmail.com>
2026-05-23 16:55:27 +0530

    refactor: improve pivot finding logic in pivot.cpp

--- Sorting/questions/pivot.cpp
+++ Sorting/questions/pivot.cpp
@@ -4,15 +4,17 @@ using namespace std;
 int findPivot(int arr[],int n){
     int s = 0;
     int e = n-1;
-    int mid = s+(e-s)/2;
+
+    int mid = s + (e-s)/2;
 
     while(s<e){
-        if(arr[mid]>=arr[0]){
-            s=mid+1;
+        if(arr[0]<arr[mid]){
+            s = mid + 1;
         }
         else{
-            e=mid;
+            e = mid;
         }
+
         mid = s + (e-s)/2;
     }
 

b1859d3b957e5944c0f87b533a940fd9f0bd6d46
18gourav <gouravgarg993@gmail.com>
2026-06-13 01:09:17 +0530

    did some tree ques and end it

---
+++ BinaryTrees/Questions/flattenTreeToLL.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    void flatten(Node *root) {
+        // code here
+        Node* curr = root;
+        
+        while(curr != NULL){
+            if(curr->left){
+                Node* prev = curr->left;
+                while(prev->right){
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            
+            curr = curr->right;
+            
+        }
+        
+    }
+};

bd2fe731904e7e03599d69ceb28f885173c6ea7f
18gourav <gouravgarg993@gmail.com>
2026-07-08 01:02:54 +0530

    feat: implement cycle detection in directed graphs using BFS

--- Graphs/questions/cycleInDirected.cpp
+++ Graphs/questions/cycleInDirected.cpp
@@ -45,3 +45,60 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
 
   return 0;
 }
+
+//###########################
+//This is the approach using BFS(kanh algo)
+//###########################
+
+#include<list>
+#include<queue>
+int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
+  //create adjacency list
+    unordered_map<int,list<int>> adjList;
+    for(int i=0; i<edges.size(); i++){
+        int u = edges[i].first - 1;
+        int v = edges[i].second - 1;
+
+        adjList[u].push_back(v);
+    }
+
+    //now find indegree of all vertices
+    vector<int> indDegree(n);
+    for(auto i:adjList){
+        for(auto j:i.second){
+            indDegree[j]++;
+        }
+    }
+
+    //0 indegree valo ko push kardo queue ma
+    queue<int> q;
+    for(int i=0; i<indDegree.size(); i++){
+        if(indDegree[i] == 0){
+            q.push(i);
+        }
+    }
+
+    //ab normal bfs karo
+    int count = 0;
+
+    while(!q.empty()){
+        int top = q.front();
+        q.pop();
+
+        count++;
+
+        for(auto neighbour: adjList[top]){
+            indDegree[neighbour]--;
+
+            if(indDegree[neighbour] == 0){
+                q.push(neighbour);
+            }
+        }
+    }
+
+    if(count != n){
+      return 1;
+    }
+
+    return 0;
+}
